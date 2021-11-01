141f2fa166ccdc073d1b655b5e9abc870bba59e5
mcuneytunsal <munsal1@binghamton.edu>
2021-10-31 21:01:24 -0400

    generated api key from openweatherapi.com added city value and api value to MainActivity.kt

--- weather2/app/src/main/java/com/example/weather2/MainActivity.kt
+++ weather2/app/src/main/java/com/example/weather2/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.weather2
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val CITY: String = "istanbul,tr"
+    val API: String = "c76860c21cacc1b856fe3519b6835ce8" // Use API key
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //started now
+
     }
+
 }
