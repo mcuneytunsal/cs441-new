9a30c451289b4623c46455afb9bae866fd4a88ea
mcuneytunsal <munsal1@binghamton.edu>
2021-10-31 21:20:34 -0400

    added weatherTask(), really early impelementation

--- weather2/app/src/main/java/com/example/weather2/MainActivity.kt
+++ weather2/app/src/main/java/com/example/weather2/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.weather2
 
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        weatherTask().execute()
     }
 
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).visibility = View.GONE
+        }
 }
