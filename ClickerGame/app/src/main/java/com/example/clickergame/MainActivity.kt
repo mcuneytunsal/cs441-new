f42ac172531d1e0770d2d367eb142b73e5aa80f3
mcuneytunsal <munsal1@binghamton.edu>
2021-11-15 14:29:19 -0500

    More variables this commit: -added buttons and textviews in layout -added functionality to them -added gamelogic for 2 buttons, will improve tho

--- ClickerGame/app/src/main/java/com/example/clickergame/MainActivity.kt
+++ ClickerGame/app/src/main/java/com/example/clickergame/MainActivity.kt
@@ -1,30 +1,93 @@
 package com.example.clickergame
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
+
 class MainActivity : AppCompatActivity() {
 
-    //currentGold variable set to 0
+    //variables are set here
     private var currentGold=0
-    private var increment=1
+    private var currentStage=1
+    private var currentClickPower=1
+    private var currentIdlePower=0
+    private var currentPrestige=0
+
+    private var firstClickUpgrade=25
+    private var firstIdleUpgrade=300
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //ImageView set as imgEnemy
+        //values are set here
         val imgEnemy: ImageView = findViewById(R.id.imgEnemy)
         val showGold: TextView= findViewById(R.id.currentGold)
+        val btnClickPower: Button=findViewById(R.id.upClick)
+        val showClickPower: TextView=findViewById(R.id.currentClickPower)
+        val btnIdlePower: Button= findViewById(R.id.upIdle)
+        val showIdlePower: TextView=findViewById(R.id.currentIdlePower)
+        val btnPrestige: Button= findViewById(R.id.upPrestige)
+        val showPrestige: TextView =findViewById(R.id.currentPrestige)
+
+        val showStage:TextView=findViewById(R.id.currentStage)
+        showStage.text="$currentStage"
 
         //increments gold in each click
         imgEnemy.setOnClickListener{
-            currentGold += increment
+            currentGold += currentClickPower
             showGold.text="$currentGold"
         }
+
+        //upgrades click power
+        btnClickPower.setOnClickListener{
+            if (currentGold>=firstClickUpgrade) {
+                currentGold -= firstClickUpgrade
+                currentClickPower++
+                firstClickUpgrade+=firstClickUpgrade
+                showClickPower.text = "$currentClickPower"
+                //always update current gold
+                showGold.text="$currentGold"
+            }
+            else{
+                Toast.makeText(this,"Need More Gold!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //upgrades idle power
+        btnIdlePower.setOnClickListener{
+            if (currentGold>=firstIdleUpgrade) {
+                currentGold-=firstIdleUpgrade
+                currentIdlePower++
+                firstIdleUpgrade+=firstIdleUpgrade
+                showIdlePower.text = "$currentIdlePower"
+                //always update current gold
+                showGold.text = "$currentGold"
+            }
+            else{
+                Toast.makeText(this,"Need More Gold!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //upgrades Prestige
+        btnPrestige.setOnClickListener{
+            if (currentStage>=10) {
+                currentPrestige++
+                showPrestige.text = "$currentPrestige"
+                //always update current gold
+                showGold.text = "$currentGold"
+            }
+            else{
+                Toast.makeText(this,"Reach Stage 10 to unlock Prestige!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
